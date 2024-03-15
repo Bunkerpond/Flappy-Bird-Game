161e7b11129595fd9063b264b6c3d1c8594b4c77
Ilya Abbasian <iabbasiyanko23@gmail.com>
2024-03-14 23:31:29 -0400

    p10 done/better way of backround
    finished part 10 and found a better way to have images for background be loaded in.

--- src/index.js
+++ src/index.js
@@ -28,7 +28,8 @@ function create(){
 //x-400
 //y-300
 //key of the image 
-  this.add.image(config.width/2, config.height/2, 'sky');
+  //this.add.image(config.width/2, config.height/2, 'sky'); <------ One way to do it but below is better
+  this.add.image(0,0, 'sky').setOrigin(0);
 }
 
 new Phaser.Game(config);
