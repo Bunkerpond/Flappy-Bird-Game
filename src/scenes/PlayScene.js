d7d11083c1fb961371cd9d764b5209ed12a2a29e
Ilya Abbasian <iabbasiyanko23@gmail.com>
2024-03-17 11:37:59 -0400

    bird spritesheet

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -91,7 +91,10 @@ class PlayScene extends BaseScene {
     }
 
     createBird(){
-        this.bird = this.physics.add.sprite(this.config.startPosition.x, this.config.startPosition.y, 'bird').setOrigin(0);
+        this.bird = this.physics.add.sprite(this.config.startPosition.x, this.config.startPosition.y, 'bird')
+        .setFlipX(true)
+        .setScale(2.5)
+        .setOrigin(0);
         this.bird.body.gravity.y = 600;
         this.bird.setCollideWorldBounds(true);
     }
